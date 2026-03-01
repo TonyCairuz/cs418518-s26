c65dc370b40b5ac67a7922e8905b7933f2a84908
TonyCairuz <162224221+TonyCairuz@users.noreply.github.com>
2026-03-01 20:23:26 +0000

    add mising imports

--- Project/client/src/VerifyEmail.jsx
+++ Project/client/src/VerifyEmail.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
 export default function VerifyEmail() {
